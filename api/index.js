9637a57c048094905d2a17177afa8fdd0007a77e
iamus4ma <usama.hassan@simplexmed.com>
2024-01-16 18:55:04 +0500

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from './routes/user.route.js';
 
 mongoose
-  .connect("mongodb+srv://iamus4ma:iamus4ma@mern-estate.znrq6s5.mongodb.net/mern-estate?retryWrites=true&w=majority")
+  .connect(
+    "mongodb+srv://iamus4ma:iamus4ma@mern-estate.znrq6s5.mongodb.net/mern-estate?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use('/api/user', userRouter);
