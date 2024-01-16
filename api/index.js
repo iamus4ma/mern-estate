5132db8cbba219fe4de6347bbf37c1bbcda905b2
iamus4ma <usama.hassan@simplexmed.com>
2024-01-16 16:52:13 +0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect("mongodb+srv://iamus4ma:iamus4ma@mern-estate.znrq6s5.mongodb.net/mern-estate?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
