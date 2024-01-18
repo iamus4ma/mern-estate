5a9b76b38d63551ba251c6c661581d893a186d6d
iamus4ma <usama.hassan@simplexmed.com>
2024-01-18 23:00:50 +0500

    completed update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { React, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 import {
   getDownloadURL,
   getStorage,
@@ -7,20 +8,44 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice.js";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef();
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     if (file) {
       handleFileUpload();
     }
   }, [file]);
+
+  useEffect(() => {
+    if (formData.avatar) {
+      setValue("avatar", formData.avatar);
+    } else {
+      setValue("avatar", currentUser.avatar);
+    }
+  }, [formData.avatar, currentUser.avatar]);
 
   const handleFileUpload = () => {
     if (!file) {
@@ -51,10 +76,37 @@ export default function Profile() {
     );
   };
 
+  const onSubmit = async (formData) => {
+    const filteredFormData = Object.fromEntries(
+      Object.entries(formData).filter(([key, value]) => value !== "")
+    );
+
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(filteredFormData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <input
           type="file"
           onChange={(e) => setFile(e.target.files[0])}
@@ -64,10 +116,10 @@ export default function Profile() {
         />
         <img
           onClick={() => fileRef.current.click()}
-          className="rounded-full h-24 w-24 object-cover cursor-pointer
-          self-center mt-2"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
           src={formData.avatar || currentUser.avatar}
-          alt="profie"
+          alt="profile"
+          {...register("avatar")}
         />
         <p className="text-sm self-center">
           {fileUploadError ? (
@@ -85,33 +137,60 @@ export default function Profile() {
 
         <input
           type="text"
+          defaultValue={currentUser.username}
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          {...register("username")}
         />
+        {errors.username && (
+          <span className="text-red-700">{errors.username.message}</span>
+        )}
+
         <input
           type="email"
+          defaultValue={currentUser.email}
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          {...register("email", {
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Invalid email address",
+            },
+          })}
         />
+        {errors.email && (
+          <span className="text-red-700">{errors.email.message}</span>
+        )}
+
         <input
-          type="text"
+          type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          {...register("password")}
         />
+        {errors.password && (
+          <span className="text-red-700">{errors.password.message}</span>
+        )}
+
         <button
-          className="bg-slate-700 text-white rounded-lg p-3
-        uppercase hover:opacity-95 disabled:opacity-80"
+          type="submit"
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
         >
-          update
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      <p className="text-red-700 mt-5 jusify-center">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5 justify-center">
+        {updateSuccess && !error ? "User is updated successfully!" : ""}
+      </p>
     </div>
   );
 }
