392031348b1b1f5801f614a633a129ef02845c9e
iamus4ma <usama.hassan@simplexmed.com>
2024-01-18 21:50:41 +0500

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -21,6 +21,9 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
