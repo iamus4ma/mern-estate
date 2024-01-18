36595e8c1c60227f8a5ce0766807087a37e3d1ea
iamus4ma <usama.hassan@simplexmed.com>
2024-01-18 16:15:29 +0500

    make the profile page private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,7 +42,7 @@ export const signin = async (req, res, next) => {
 
 export const google = async (req, res, next) => {
   try {
-    const user = await User.findOne9({ email: req.body.email });
+    const user = await User.findOne({ email: req.body.email });
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
@@ -52,8 +52,7 @@ export const google = async (req, res, next) => {
         .status(200)
         .json(rest);
     } else {
-      const generaredPassword = Math.random.toString(36).slice(-8);
-      +Math.random.toString(36).slice(-8);
+      const generaredPassword = Math.random.toString(36).slice(-8) + Math.random.toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generaredPassword, 10);
       const newUser = new User({
         username:
