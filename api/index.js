9c0c2f66d6307d6ae8d6831cb4598034dd50df57
iamus4ma <usama.hassan@simplexmed.com>
2024-01-19 17:25:31 +0500

    create api done

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -30,6 +32,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
