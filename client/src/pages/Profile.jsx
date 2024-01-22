b2770cb0f001ab1bbbc1a3c9e57bdde74960816b
iamus4ma <usama.hassan@simplexmed.com>
2024-01-22 19:38:52 +0500

    complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -28,6 +28,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState();
   const dispatch = useDispatch();
 
   const {
@@ -110,6 +112,24 @@ export default function Profile() {
       dispatch(deleteUserFailure(data.message));
     }
   };
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingError(true);
+    }
+  };
+
+  const handleListingDelete = () => {};
 
   const onSubmit = async (formData) => {
     const filteredFormData = Object.fromEntries(
@@ -228,18 +248,70 @@ export default function Profile() {
       <div className="flex justify-between mt-5">
         <span
           onClick={handleDeleteUser}
-          className="text-red-700 cursor-pointer"
+          className="text-red-700 cursor-pointer hover:font-semibold"
         >
           Delete account
         </span>
-        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+        <span
+          onClick={handleSignOut}
+          className="text-red-700 cursor-pointer hover:font-semibold"
+        >
           Sign out
         </span>
       </div>
-      <p className="text-red-700 mt-5 jusify-center">{error ? error : ""}</p>
-      <p className="text-green-700 mt-5 justify-center">
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5 ">
         {updateSuccess && !error ? "User is updated successfully!" : ""}
       </p>
+      <button
+        onClick={handleShowListings}
+        type="button"
+        className=" text-green-700 w-full hover:font-semibold"
+      >
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingError ? "Error showing listings" : ""}
+      </p>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col item-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                {/* <Link to={`/update-listing/${listing._id}`}> */}
+                  <button className="text-green-700 uppercase">Edit</button>
+                {/* </Link> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
