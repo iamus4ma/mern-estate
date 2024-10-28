900dd3e24997bb59cc070ed5b11dbcef3bc1656a
iamus4ma <iamus4ma@gmail.com>
2024-10-28 15:06:34 +0500

    deploy changes added

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
-
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -21,6 +21,8 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 
@@ -33,6 +35,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
