57af11fddd1227a8607052928d565b36d7f1af69
iamus4ma <iamus4ma@gmail.com>
2024-10-29 12:54:49 +0500

    reverted index changes

--- api/index.js
+++ api/index.js
@@ -9,53 +9,45 @@ import path from "path";
 
 dotenv.config();
 
-const __dirname = path.resolve();
-
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-
-// Disable buffering and set buffer timeout
-mongoose.set('bufferCommands', false);       // Disable Mongoose buffering
-mongoose.set('bufferTimeoutMS', 60000);      // Set buffer timeout to 1 minute
-
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 60000, // 1 minute
-    socketTimeoutMS: 60000,          // 1 minute
   })
   .then(() => {
     console.log("Connected to MongoDB");
-
-    // Only start handling routes after successful connection
-    app.use("/api/user", userRouter);
-    app.use("/api/auth", authRouter);
-    app.use("/api/listing", listingRouter);
-
-    app.use(express.static(path.join(__dirname, "/client/dist")));
-
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-    });
-
-    app.use((err, req, res, next) => {
-      const statusCode = err.statusCode || 500;
-      const message = err.message || "Internal Server Error";
-      return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-      });
-    });
-
-    // Start server after connection
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
-    });
   })
   .catch((err) => {
-    console.log("Error connecting to MongoDB:", err);
+    console.log(err);
   });
+
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
